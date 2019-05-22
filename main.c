7d2982be55ab41086b1114f5487574020fe51d46
Jeff22096 <50893548+Jeff22096@users.noreply.github.com>
2019-05-21 21:23:58 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,30 @@ int main()
   }
   return 0;
 }
+int main()
+{
+  int i;
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+int main()
+{
+  int i;
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+int main()
+{
+  int i;
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
